afbc0fb179f026009239c71049980784ec2957c0
Mesezi <94804619+Mesezi@users.noreply.github.com>
2022-02-26 16:47:42 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -40,8 +40,7 @@ var showLoading = function (selector) {
 // with propValue in given 'string'
 var insertProperty = function (string, propName, propValue) {
   var propToReplace = "{{" + propName + "}}";
-  string = string
-    .replace(new RegExp(propToReplace, "g"), propValue);
+  string = string.replace (new RegExp(propToReplace, "g"), propValue);
   return string;
 };
 
@@ -102,11 +101,11 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
-      var chosenCategoryShortName = chooseRandomCategory (categories);
-      chosenCategoryShortName = '"'+ chosenCategoryShortName +'"'
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, randomCategoryShortName, chosenCategoryShortName);
+      var chosenCategoryShortName = chooseRandomCategory (categories).short_name;
+      chosenCategoryShortName = "'"+ chosenCategoryShortName + "'";
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
   
-      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+      insertHtml('#main-content', homeHtmlToInsertIntoMainPage);
 
   },
    
